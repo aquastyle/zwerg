e5ab2607e1d76c8f8aee83c50cce21007154e34d
Эдуард Заворитний <eduardzavoritnij@MacBook-Air-Eduard.local>
2022-12-23 20:51:35 +0200

    fiex

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,16 @@
 
 $(document).ready(function() {
-  $(".menuToggle").click(function() {
+  $(".burger").click(function() {
     $(this).toggleClass("active");
+    $("body").toggleClass("no-scroll");
     $('.menu').slideToggle(300, function(){
       if($(this).css('display') === "none"){
         $(this).removeAttr('style');
       }
     });
   });
+
+  $('.form__input-phone').mask('+380 (00) 000-00-00');
 });
 
 
